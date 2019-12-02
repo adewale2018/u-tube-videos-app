a0317c7e6e4636a3db127b38c09dab68a5aba0fb
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2019-12-02 21:36:37 +0100

    Ädd search bar component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: "" };
+  }
+  handleChange = e => {
+    this.setState({ term: e.target.value });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    if(this.state.term) {
+      this.props.seachTerm(this.state.term); 
+    }
+  }
+  render() {
+    return (
+      <div className='search-bar ui segment'>
+        <form className='ui form' onSubmit={this.handleSubmit}>
+          <div className='field'>
+            <label>Search for videos</label>
+            <input
+              type='text'
+              value={this.state.term}
+              onChange={this.handleChange}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
