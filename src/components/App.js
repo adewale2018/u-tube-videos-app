6b9a44e906277f6ea373f0f3c3735b655a8c4531
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2019-12-03 17:22:43 +0100

    accessing the youtube api

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,32 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import YouTube, { baseParams } from "./api/YouTube";
+import VideoList from './VideoList';
 
-const App = () => {
-  return (
-    <div className="ui container" style={{marginTop: "20px"}}>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = { videos: [] };
+  }
+  onTermSubmit = async term => {
+    const response = await YouTube.get("/search", {
+      params: {
+        ...baseParams,
+        q: term
+      }
+    });
+
+    this.setState({ videos: response.data.items });
+    console.log(this.state.videos);
+  };
+  render() {
+    return (
+      <div className='ui container' style={{ marginTop: "20px" }}>
+        <SearchBar onTermSubmit={this.onTermSubmit} />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
 export default App;
---
+++ src/components/VideoList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import VideoItem from './VideoItem';
+
+const VideoList = ({ videos }) => {
+  return (
+    <div>
+      {videos.map(video => (
+        <VideoItem key={video.etag} video={video} />
+      ))}
+    </div>
+  );
+};
+
+export default VideoList;
